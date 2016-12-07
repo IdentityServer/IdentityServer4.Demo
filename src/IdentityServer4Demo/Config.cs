898e2ecceeafb7a404131ea4a4ba42823b25366f
Dominick Baier <dbaier@leastprivilege.com>
2016-12-07 15:15:36 +0100

    initial commit

---
+++ src/IdentityServer4Demo/Config.cs
@@ -0,0 +1,73 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Services.InMemory;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace IdentityServer4Demo
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1", "My API #1")
+            };
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    ClientSecrets = { new Secret("secret".Sha256() )},
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = { "api1" }
+                },
+
+                new Client
+                {
+                    ClientId = "mvc",
+                    ClientName = "MVC application",
+
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+                    RedirectUris = { "http://localhost:5001/signin-oidc" },
+
+                    AllowedGrantTypes = GrantTypes.Hybrid,
+                    AllowedScopes = { "openid", "profile", "api1" }
+                }
+            };
+        }
+
+        public static List<InMemoryUser> GetUsers()
+        {
+            return new List<InMemoryUser>
+            {
+                new InMemoryUser
+                {
+                    Subject = "1",
+                    Username = "bob",
+                    Password = "bob",
+
+                    Claims =
+                    {
+                        new Claim("name", "Bob Smith")
+                    }
+
+                }
+            };
+        }
+    }
+}
---
+++ src/IdentityServer4Demo/Startup.cs
@@ -0,0 +1,57 @@
+﻿using IdentityServer4;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace IdentityServer4Demo
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddIdentityServer()
+                .AddTemporarySigningCredential()
+                .AddInMemoryApiResources(Config.GetApis())
+                .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                .AddInMemoryClients(Config.GetClients())
+                .AddInMemoryUsers(Config.GetUsers());
+        }
+
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            var serilog = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .Enrich.FromLogContext()
+                .WriteTo.LiterateConsole(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message}{NewLine}{Exception}{NewLine}")
+                .CreateLogger();
+
+            loggerFactory.AddSerilog(serilog);
+            app.UseDeveloperExceptionPage();
+
+            app.UseIdentityServer();
+
+            // cookie middleware for temporarily storing the outcome of the external authentication
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme,
+                AutomaticAuthenticate = false,
+                AutomaticChallenge = false
+            });
+
+            // middleware for google authentication
+            app.UseGoogleAuthentication(new GoogleOptions
+            {
+                AuthenticationScheme = "Google",
+                SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme,
+                ClientId = "434483408261-55tc8n0cs4ff1fe21ea8df2o443v2iuc.apps.googleusercontent.com",
+                ClientSecret = "3gcoTrEDPPJ0ukn_aYYT6PWo"
+            });
+
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
