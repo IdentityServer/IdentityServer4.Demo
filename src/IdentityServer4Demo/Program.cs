1ecd3ce1892afd608cc475b5c6fb757bf5aa7e5d
Dominick Baier <dbaier@leastprivilege.com>
2018-03-19 10:27:40 +0100

    Add Serilog support

--- src/IdentityServer4Demo/Program.cs
+++ src/IdentityServer4Demo/Program.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Serilog;
+using Serilog.Events;
 using System;
-using Microsoft.AspNetCore;
 
 namespace IdentityServer4Demo
 {
@@ -17,6 +19,28 @@ namespace IdentityServer4Demo
         {
             return WebHost.CreateDefaultBuilder(args)
                     .UseStartup<Startup>()
+                    .UseSerilog((ctx, config) =>
+                    {
+                        config.MinimumLevel.Debug()
+                            .MinimumLevel.Debug()
+                            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                            .MinimumLevel.Override("System", LogEventLevel.Warning)
+                            .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+                            .Enrich.FromLogContext();
+
+                        if (ctx.HostingEnvironment.IsDevelopment())
+                        {
+                            config.WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}");
+                        }
+                        else if (ctx.HostingEnvironment.IsProduction())
+                        {
+                            config.WriteTo.File(@"D:\home\LogFiles\Application\identityserver.txt",
+                                fileSizeLimitBytes: 1_000_000,
+                                rollOnFileSizeLimit: true,
+                                shared: true,
+                                flushToDiskInterval: TimeSpan.FromSeconds(1));
+                        }
+                    })
                     .Build();
         }
     }
