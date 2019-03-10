77074b67b7987c4d26066f9bb19f2562a624ceab
Dominick Baier <dbaier@leastprivilege.com>
2019-03-10 11:31:36 +0100

    update subs of alice and bob

--- src/IdentityServer4Demo/Quickstart/TestUsers.cs
+++ src/IdentityServer4Demo/Quickstart/TestUsers.cs
@@ -13,7 +13,7 @@ namespace IdentityServer4.Quickstart.UI
     {
         public static List<TestUser> Users = new List<TestUser>
         {
-            new TestUser{SubjectId = "818727", Username = "alice", Password = "alice", 
+            new TestUser { SubjectId = "1", Username = "alice", Password = "alice", 
                 Claims = 
                 {
                     new Claim(JwtClaimTypes.Name, "Alice Smith"),
@@ -25,7 +25,7 @@ namespace IdentityServer4.Quickstart.UI
                     new Claim(JwtClaimTypes.Address, @"{ 'street_address': 'One Hacker Way', 'locality': 'Heidelberg', 'postal_code': 69118, 'country': 'Germany' }", IdentityServer4.IdentityServerConstants.ClaimValueTypes.Json)
                 }
             },
-            new TestUser{SubjectId = "88421113", Username = "bob", Password = "bob", 
+            new TestUser { SubjectId = "11", Username = "bob", Password = "bob", 
                 Claims = 
                 {
                     new Claim(JwtClaimTypes.Name, "Bob Smith"),
