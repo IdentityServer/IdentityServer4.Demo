895e8aab69de9ffd1bba950a83d4a566c7070dce
Dominick Baier <dbaier@leastprivilege.com>
2020-06-19 10:06:56 +0200

    update UI

--- src/IdentityServer4Demo/Quickstart/Diagnostics/DiagnosticsController.cs
+++ src/IdentityServer4Demo/Quickstart/Diagnostics/DiagnosticsController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace IdentityServer4.Quickstart.UI
+namespace IdentityServerHost.Quickstart.UI
 {
     [SecurityHeaders]
     [Authorize]
@@ -16,6 +16,12 @@ namespace IdentityServer4.Quickstart.UI
     {
         public async Task<IActionResult> Index()
         {
+            var localAddresses = new string[] { "127.0.0.1", "::1", HttpContext.Connection.LocalIpAddress.ToString() };
+            if (!localAddresses.Contains(HttpContext.Connection.RemoteIpAddress.ToString()))
+            {
+                return NotFound();
+            }
+
             var model = new DiagnosticsViewModel(await HttpContext.AuthenticateAsync());
             return View(model);
         }
